184acb66ec8ca022d1ce3632e6f37fbd6bb91a94
DeCorben <32888104+DeCorben@users.noreply.github.com>
2021-12-22 13:40:51 -0500

    shell commences

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,8 @@
 import fastify from 'fastify';
 
 fastify()
-  .then((server) => server.get('/', (req,res) => {
+  .then((server) => server.get('/', (req, res) => {
     res.send('Hello, World!');
-  })
+  }))
   .then((server) => server.listen(4242))
-  .then(() => {console.log('Listening on 4242');});
-
+  .then(() => { console.log('Listening on 4242'); });
